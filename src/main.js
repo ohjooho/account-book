4ce687af341af157ad2a9b7524e23869fb19b40f
zziony <wldnjs1123@gmail.com>
2026-04-08 15:03:40 +0900

    chore: install vuetify and fontawesome

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+//fontawesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, far, fab);
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-const app = createApp(App)
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
