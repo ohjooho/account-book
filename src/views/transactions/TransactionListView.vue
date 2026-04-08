aa47466acdf08098cce7cff5019008ddcdbbfee0
JH JUNG <jhdream01@gmail.com>
2026-04-09 00:05:56 +0900

    feat: category store 조회 기능 구현 및 API 연동 확인

--- src/views/transactions/TransactionListView.vue
+++ src/views/transactions/TransactionListView.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
     <h1>transactions page</h1>
-    <p>{{ store.transactions.length }} 건</p>
+    <p>데이터 {{ transactionsStore.transactions.length }} 건</p>
+    <p>카테고리 {{ categoryStore.categories.length }} 개</p>
+    <p>카테고리 데이터 예시: {{ categoryStore.categories[0] }}</p>
+    <p>
+      특정 카테고리 찾기:
+      {{ categoryStore.getCategoryById('transport').labelKo }}
+    </p>
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import { useTransactionsStore } from '@/stores/transactions';
+import { useCategoryStore } from '@/stores/category';
 
-const store = useTransactionsStore();
+const transactionsStore = useTransactionsStore();
+const categoryStore = useCategoryStore();
 
-onMounted(() => {
-  store.fetchTransactions();
+onMounted(async () => {
+  await transactionsStore.fetchTransactions();
+  await categoryStore.fetchCategories();
+
+  const foodCategory = categoryStore.getCategoryById('food');
+  console.log('찾은 카테고리:', foodCategory.id);
+  console.log(foodCategory.labelKo);
 });
 </script>
