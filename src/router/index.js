542e877e5451bb30f84a6bbe8507050689153a95
Jooho Oh <jooho@Joohoui-MacBookPro.local>
2026-04-08 11:16:24 +0900

    feat: 라우터 최적화 및 영수증 데이터 구조 정규화

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,77 @@
-import DashboardView from "@/views/DashboardView.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
+    // main-001: 메인 페이지(대시보드)
     path: '/',
     name: 'dashboard',
-    component: () => import('@/views/DashboardView.vue')
+    component: () => import('@/views/DashboardView.vue'),
   },
   {
+    // tran-001, 002, 003: 거래 내역 조회
     path: '/transactions',
-    name: 'transactions',
     children: [
-      {path: '', component: () => import('@/views/transactions/TransactionListView.vue')},
-      {path: 'new', component: () => import('@/views/transactions/TransactionFormView.vue')},
-      {path: '', component: () => import('@/views/transactions/TransactionDetailView.vue')}, 
-    ]
+      {
+        path: '',
+        name: 'transactions',
+        component: () => import('@/views/transactions/TransactionListView.vue'),
+      },
+      // tran-002: 내역 작성
+      {
+        path: 'new',
+        name: 'transactions-create',
+        component: () => import('@/views/transactions/TransactionFormView.vue'),
+      },
+      // tran-003: 내역 상세(수정, 삭제 포함)
+      {
+        path: ':id',
+        name: 'transactions-detail',
+        component: () =>
+          import('@/views/transactions/TransactionDetailView.vue'),
+      },
+    ],
   },
   {
-    path: '/analysis/:date',
+    // analysis-001: 월별 분석
+    path: '/monthly',
     name: 'analysis',
-    component: () => import('@/views/AnalysisView.vue')
+    component: () => import('@/views/AnalysisView.vue'),
   },
   {
+    // receipt-001, 002: AI 영수증 첨부 및 등록
     path: '/receipt',
-    name: 'receipt',
     children: [
-      {path: '', component: () => import('@/views/receipt/ReceiptUploadView.vue')},
-      {path: 'new', component: () => import('@/views/receipt/ReceiptConfirmView.vue')}
-    ]
+      // receipt-001
+      {
+        path: '',
+        name: 'receipt-upload',
+        component: () => import('@/views/receipt/ReceiptUploadView.vue'),
+      },
+      // receipt-002
+      {
+        path: ':receiptId',
+        name: 'receipt-confirm',
+        component: () => import('@/views/receipt/ReceiptConfirmView.vue'),
+      },
+    ],
   },
   {
+    // report-001
     path: '/report',
     name: 'report',
-    component: () => import('@/views/ReportView.vue')
+    component: () => import('@/views/ReportView.vue'),
   },
   {
+    // map-001
     path: '/map',
     name: 'map',
-    component: () => import('@/views/MapView.vue')
-  }
-]
+    component: () => import('@/views/MapView.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 export default router;
-
